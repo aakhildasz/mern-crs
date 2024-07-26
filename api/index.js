186d60898e715fc4485dcaa7828466695c468e6d
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-07-26 23:30:42 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 
 import dotenv from "dotenv";
+import userRouter from "./routes/user.routes.js";
 dotenv.config();
 
 mongoose
@@ -19,3 +20,5 @@ const port = 7000;
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}/`);
 });
+
+app.use("/api/user", userRouter);
