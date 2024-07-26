20b2563245edfca72b68716dcaf98133a1a47f06
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-07-27 03:01:58 +0530

    complete sign up page functionality

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:7000",
+        secure: false,
+      },
+    },
     host: "0.0.0.0",
     port: 3557,
   },
