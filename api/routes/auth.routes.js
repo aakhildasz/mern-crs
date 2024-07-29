d0671d1fd3fc0cbd06629a454a9da68b9cb02f03
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-07-30 02:17:28 +0530

    add sign out functionality

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { signin, signup, google } from "../controllers/auth.controllers.js";
+import {
+  signin,
+  signup,
+  google,
+  singout,
+} from "../controllers/auth.controllers.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/signup", signup);
 authRouter.post("/signin", signin);
 authRouter.post("/google", google);
+authRouter.get("/signout", singout);
 
 export default authRouter;
