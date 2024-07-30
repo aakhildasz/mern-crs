d394b9840adfe2d3d6a2a403c682c5d55d160087
Akhil Das <113711306+aakhildasz@users.noreply.github.com>
2024-07-31 00:28:46 +0530

    some changes

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -9,7 +9,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.get("/test", test);
-router.get("/update/:id", verifyToken, updateUser);
+router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
 export default router;
